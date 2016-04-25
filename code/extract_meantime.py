085124f9bd6ac3380606ad0ab5c307a4ae7fbb86
Rohit Sinha <sinharo@gmail.com>
2016-04-25 04:11:21 +0000

    fix

--- code/extract_meantime.py
+++ code/extract_meantime.py
@@ -4,18 +4,23 @@
 import sys
 from os import listdir
 from os.path import isfile, join
-
+import bz2
 
 def extract_raw(s):
-	return s.split("<raw>")[1].split("</raw>")[0]
+    result = s.split("<raw>")[1].split("</raw>")[0]
+    if "![CDATA[" in result:
+        result = result.split("![CDATA[")[1].split("]]>")[0]
+    return result
 
 input_path = sys.argv[1]
 output_path = sys.argv[2] + "/"
 
 inputs = [f for f in listdir(input_path) if isfile(join(input_path, f)) and ".naf" in f]
 for f in inputs:
-	output_filename = f.split(".naf")[0] + ".txt"
-	content = open(f).read()
-	output_file = open(output_path + output_filename, 'w')
-	output_file.write(extract_raw(content))
-	output_file.close()
+    output_filename = f.split(".naf")[0] + ".txt"
+    content = open(input_path + "/" + f).read()
+    if ".bz2" in f:
+        content = bz2.decompress(content)
+    output_file = open(output_path + output_filename, 'w')
+    output_file.write(extract_raw(content))
+    output_file.close()
