a61daf6f4cf7851873d19459ddb6d8b90a07f02b
juanshishido <juanshishido@cal.berkeley.edu>
2016-05-01 00:32:17 -0700

    `represent()` docstring and lexical features function

--- code/utils.py
+++ code/utils.py
@@ -61,7 +61,20 @@ def tokenizer():
     return RegexpTokenizer("(?:[A-Za-z]\.)+|\w+(?:-\w+)*|\$[\d\.]+|\.\.\.|\S+")
 
 def represent(train, test, as_binary=True, tokenizer=None, vocabulary=None):
-    """
+    """Feature representation: binary or bi-normal separation feature scaling
+
+    Parameters
+    ----------
+    train : pd.DataFrame
+        Training data
+    test : pd.DataFrame
+        Testing data
+    as_binary : bool
+        Binary or bi-normal separation feature scaling
+    tokenizer : callable or None (default)
+        For string tokenization
+    vocabulary : list
+
     Return
     ------
     X_train, X_test : np.ndarray
@@ -89,13 +102,39 @@ def represent(train, test, as_binary=True, tokenizer=None, vocabulary=None):
                      vocabulary=vocabulary)
     return X_train, X_test
 
-def nmf(fit, transform):
-    model = NMF(n_components=5, random_state=42)
-    model.fit(fit)
-    H = model.transform(transform)
-    labels = np.argmax(H, axis=1)
-    nmf_dummies = pd.get_dummies(pd.Series(labels)).values
-    return nmf_dummies
+def features_lexical(X, data, tokenizer=None):
+    """Lexical features: profanity and slang
+
+    Parameters
+    ----------
+    X : scipy.sparse.csr.csr_matrix
+        Feature representation
+    data : pd.DataFrame
+        Training or testing data
+    tokenizer : callable or None (default)
+        For string tokenization
+
+    Returns
+    -------
+    lexical : scipy.sparse.csr.csr_matrix
+        shape (n_samples, 2)
+        binary representation for profanity
+        proportion of slang tokens
+    """
+    X, data = X.copy(), data.copy()
+    profane = word_lists('data/profane.txt')
+    data['profane'] = data.text.apply(lambda text: contains(profane, text))
+    slang = word_lists('lists/slang.txt')
+    cv = CountVectorizer(vocabulary=slang, tokenizer=tokenizer)
+    slang_counts = cv.fit_transform(data.text.values)
+    lexical = hstack([csr_matrix(data.profane.values).T,
+                      np.divide(slang_counts.sum(axis=1), X.sum(axis=1))],
+                     format='csr')
+    return lexical
+
+def features_validity():
+    """Validity-based features"""
+    return NotImplemented
 
 def append_features(X, data, include='all', tokenizer=None):
     assert include in ('lex', 'val', 'all', 'none'), 'Not a valid option'
@@ -127,7 +166,16 @@ def append_features(X, data, include='all', tokenizer=None):
                    format='csr')
     return X
 
+def nmf(fit, transform):
+    model = NMF(n_components=5, random_state=42)
+    model.fit(fit)
+    H = model.transform(transform)
+    labels = np.argmax(H, axis=1)
+    nmf_dummies = pd.get_dummies(pd.Series(labels)).values
+    return nmf_dummies
+
 def tune_params(X, y, clf, grid):
+    np.random.seed(42)
     grid_search = GridSearchCV(clf, grid, cv=5, n_jobs=-1)
     grid_search.fit(X, y)
     return grid_search.best_params_
